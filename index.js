cf9300d0bbb1c5b395a5dfc9636acbe474ed6cc5
ZoeyNguyen65 <hanhngoc9956@gmail.com>
2025-01-26 16:11:55 -0600

    modify index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Your code here
+function greet() {
+
+username = prompt("What is your name?");
+if (username === null || username.trim() === "") return;// To exit the function if user chooses "Cancel" or leave the space blank
+
+alert("Hello, " + username);
+
+age = parseInt(prompt("How old are you?"));
+if (isNaN(age)) return;
+
+hadABirthday = confirm("Have you had a birthday yet this year? Click OK for Yes, Cancel for No");
+
+currentYear = new Date().getFullYear();
+
+if (hadABirthday) {
+    birthYear = currentYear - age;
+    } 
+    else {
+    birthYear = currentYear - age - 1;
+    }
+
+alert("You were born in " + birthYear);
+}
